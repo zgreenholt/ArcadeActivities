9d9c550c9f65ded0fa8978cdb22cc5da908b3b71
zgreenholt <zgrnhlt@udel.edu>
2019-11-12 14:28:52 -0500

    worked on red dot game

--- game2_red_dot_fetch.py
+++ game2_red_dot_fetch.py
@@ -1,36 +1,60 @@
 import arcade
 import random
 
-
 # Define constants
 WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 500
 BACKGROUND_COLOR = arcade.color.BLACK
 GAME_TITLE = "Red_Dot_Fetch"
-GAME_SPEED = 1/60
+DOT_SIZE = 30
+DOT_IMAGE = arcade.make_soft_circle_texture(DOT_SIZE, arcade.color.RED, 255, 120)
+
 
 class dot(arcade.Sprite):
     def __init__(self):
-        super().__init__("images/dot.png")
-        self.center_x = random.randint(1,500)
-        self.center_y = random.randint(1,500)
+        super().__init__()
+        self.texture = DOT_IMAGE
+        self.center_x = random.randint(0, WINDOW_WIDTH)
+        self.center_y = random.randint(0, WINDOW_HEIGHT)
+        self.velocity = [random.randint(-4, 4), random.randint(-4, 4)]
+
+    def on_update(self):
+        if self.right > WINDOW_WIDTH:
+            self.change_x *= -1
+        if self.left < 0:
+            self.change_x *= -1
+        if self.top > WINDOW_HEIGHT:
+            self.change_y *= -1
+        if self.bottom < 0:
+            self.change_y *= -1
+
+
+red_dot = dot()
+
 
 class red_dot_fetch(arcade.Window):
     def __init__(self):
-        """ Initialize variables """
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE)
+        self.red_dots = None
 
     def setup(self):
         """ Setup the game (or reset the game) """
         arcade.set_background_color(BACKGROUND_COLOR)
+        self.red_dots = arcade.SpriteList()
+        self.red_dots.append(red_dot)
 
     def on_draw(self):
         """ Called when it is time to draw the world """
         arcade.start_render()
+        self.red_dots.draw()
 
     def on_update(self, delta_time):
         """ Called every frame of the game (1/GAME_SPEED times per second)"""
 
+    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
+        if dot.collides_with_point(x, y):
+            self.red_dots.append(red_dot)
+            self.red_dots.draw
 
 
 def main():
