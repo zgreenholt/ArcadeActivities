60c2a9bee976fb88051736655123594e9732ee91
zgreenholt <zgrnhlt@udel.edu>
2019-11-14 12:47:15 -0500

    completed red dot game

--- game2_red_dot_fetch.py
+++ game2_red_dot_fetch.py
@@ -10,15 +10,16 @@ DOT_SIZE = 30
 DOT_IMAGE = arcade.make_soft_circle_texture(DOT_SIZE, arcade.color.RED, 255, 120)
 
 
-class dot(arcade.Sprite):
-    def __init__(self):
+class Dot(arcade.Sprite):
+    def __init__(self, x=0, y=0):
         super().__init__()
         self.texture = DOT_IMAGE
-        self.center_x = random.randint(0, WINDOW_WIDTH)
-        self.center_y = random.randint(0, WINDOW_HEIGHT)
+        self.center_x = x
+        self.center_y = y
         self.velocity = [random.randint(-4, 4), random.randint(-4, 4)]
 
-    def on_update(self):
+    def update(self):
+        super().update()
         if self.right > WINDOW_WIDTH:
             self.change_x *= -1
         if self.left < 0:
@@ -29,10 +30,11 @@ class dot(arcade.Sprite):
             self.change_y *= -1
 
 
-red_dot = dot()
 
 
 class red_dot_fetch(arcade.Window):
+    red_dots: arcade.SpriteList
+
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE)
         self.red_dots = None
@@ -41,7 +43,7 @@ class red_dot_fetch(arcade.Window):
         """ Setup the game (or reset the game) """
         arcade.set_background_color(BACKGROUND_COLOR)
         self.red_dots = arcade.SpriteList()
-        self.red_dots.append(red_dot)
+        self.red_dots.append(Dot(random.randint(0, WINDOW_WIDTH), random.randint(0, WINDOW_HEIGHT)))
 
     def on_draw(self):
         """ Called when it is time to draw the world """
@@ -50,11 +52,15 @@ class red_dot_fetch(arcade.Window):
 
     def on_update(self, delta_time):
         """ Called every frame of the game (1/GAME_SPEED times per second)"""
+        self.red_dots.update()
 
     def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
-        if dot.collides_with_point(x, y):
-            self.red_dots.append(red_dot)
-            self.red_dots.draw
+        copy_of_red_dots = self.red_dots[:]
+        for dot in copy_of_red_dots:
+            if dot.collides_with_point([x, y]):
+                dot.remove_from_sprite_lists()
+                self.red_dots.append(Dot(x, y-50))
+                self.red_dots.append(Dot(x, y+50))
 
 
 def main():
